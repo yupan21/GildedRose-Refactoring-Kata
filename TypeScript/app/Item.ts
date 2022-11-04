928adb4d0e8b2146b95f6234944691bc6464f30d
yupan21 <yupan21@163.com>
2022-11-04 21:08:27 +0800

    抽取decreaseQuality方法

--- TypeScript/app/Item.ts
+++ TypeScript/app/Item.ts
@@ -17,15 +17,17 @@ export class Item {
   }
 
   protected updateQuality(item: Item) {
+    this.decreaseQuality();
+  }
+
+  private decreaseQuality() {
     if (this.quality > 0) {
       this.quality = this.quality - 1;
     }
   }
 
   protected updateQualityAfterExpired(item: Item) {
-    if (this.quality > 0) {
-      this.quality = this.quality - 1;
-    }
+    this.decreaseQuality();
   }
 
   private isExpired(item: Item) {
