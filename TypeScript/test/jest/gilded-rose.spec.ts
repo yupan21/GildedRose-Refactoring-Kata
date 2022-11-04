9fa74ff8737adbfa2b21097d541b76b606a13505
yupan21 <yupan21@163.com>
2022-11-04 20:11:15 +0800

    引入子类

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,18 +1,21 @@
+import { AgedBrie } from '@/AgedBrie';
+import { Backstage } from '@/BackStage';
 import { GildedRose } from '@/gilded-rose';
 import { Item } from "@/Item";
+import { Sulfuras } from '@/Sulfuras';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
     let outPutStr: String = "";
     const items = [
       new Item("+5 Dexterity Vest", 10, 20), //
-      new Item("Aged Brie", 2, 0), //
+      new AgedBrie(2, 0), //
       new Item("Elixir of the Mongoose", 5, 7), //
-      new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Sulfuras(0, 80), //
+      new Sulfuras(-1, 80),
+      new Backstage(15, 20),
+      new Backstage(10, 49),
+      new Backstage(5, 49),
       // this conjured item does not work properly yet
       new Item("Conjured Mana Cake", 3, 6)];
     
