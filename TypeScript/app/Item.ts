773fa481ccffed43a7b11fe226bf04ff74e145ed
yupan21 <yupan21@163.com>
2022-11-04 17:50:21 +0800

    抽取方法isExpired&updateQualityAfterExpired

--- TypeScript/app/Item.ts
+++ TypeScript/app/Item.ts
@@ -32,25 +32,37 @@ export class Item {
         }
       }
     }
-    if (!this.isSulfuras(item)) {
-      item.sellIn = item.sellIn - 1;
+    this.updateSellInDays(item);
+    if (this.isExpired(item)) {
+      this.updateQualityAfterExpired(item);
     }
-    if (item.sellIn < 0) {
-      if (!this.isAgedBrie(item)) {
-        if (!this.isBackStage(item)) {
-          if (item.quality > 0) {
-            if (!this.isSulfuras(item)) {
-              item.quality = item.quality - 1;
-            }
+  }
+
+  private updateQualityAfterExpired(item: Item) {
+    if (!this.isAgedBrie(item)) {
+      if (!this.isBackStage(item)) {
+        if (item.quality > 0) {
+          if (!this.isSulfuras(item)) {
+            item.quality = item.quality - 1;
           }
-        } else {
-          item.quality = item.quality - item.quality;
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+        item.quality = item.quality - item.quality;
       }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
+
+  private isExpired(item: Item) {
+    return item.sellIn < 0;
+  }
+
+  private updateSellInDays(item: Item) {
+    if (!this.isSulfuras(item)) {
+      item.sellIn = item.sellIn - 1;
     }
   }
 
