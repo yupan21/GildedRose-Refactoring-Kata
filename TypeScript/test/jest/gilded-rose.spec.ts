0ea6635b2dc7f53fa9742e71c6cb5977fadcfd84
yupan21 <yupan21@163.com>
2022-11-22 20:46:01 +0800

    构建安全网-补充单测场景覆盖

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -5,13 +5,16 @@ describe('Gilded Rose', () => {
     let outPutStr: String = "";
     const items = [
       new Item("+5 Dexterity Vest", 10, 20), //
+      new Item("+5 Dexterity Vest", 0, 20), //
       new Item("Aged Brie", 2, 0), //
+      new Item("Aged Brie", 0, 0), //
       new Item("Elixir of the Mongoose", 5, 7), //
       new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
       // this conjured item does not work properly yet
       new Item("Conjured Mana Cake", 3, 6)];
     
@@ -41,24 +44,30 @@ describe('Gilded Rose', () => {
     const expectStr: String = "-------- day 0 --------\n"+
     "name, sellIn, quality\n"+
     "+5 Dexterity Vest 10 20\n"+
+    "+5 Dexterity Vest 0 20\n"+
     "Aged Brie 2 0\n"+
+    "Aged Brie 0 0\n"+
     "Elixir of the Mongoose 5 7\n"+
     "Sulfuras, Hand of Ragnaros 0 80\n"+
     "Sulfuras, Hand of Ragnaros -1 80\n"+
     "Backstage passes to a TAFKAL80ETC concert 15 20\n"+
     "Backstage passes to a TAFKAL80ETC concert 10 49\n"+
     "Backstage passes to a TAFKAL80ETC concert 5 49\n"+
+    "Backstage passes to a TAFKAL80ETC concert 0 49\n"+
     "Conjured Mana Cake 3 6\n"+
     "-------- day 1 --------\n"+
     "name, sellIn, quality\n"+
     "+5 Dexterity Vest 9 19\n"+
+    "+5 Dexterity Vest -1 18\n"+
     "Aged Brie 1 1\n"+
+    "Aged Brie -1 2\n"+
     "Elixir of the Mongoose 4 6\n"+
     "Sulfuras, Hand of Ragnaros 0 80\n"+
     "Sulfuras, Hand of Ragnaros -1 80\n"+
     "Backstage passes to a TAFKAL80ETC concert 14 21\n"+
     "Backstage passes to a TAFKAL80ETC concert 9 50\n"+
     "Backstage passes to a TAFKAL80ETC concert 4 50\n"+
+    "Backstage passes to a TAFKAL80ETC concert -1 0\n"+
     "Conjured Mana Cake 2 5\n";
     expect(outPutStr).toBe(expectStr);
   });
